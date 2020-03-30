e53e6b497b4ba91e597f2ec539576507989af2ed
fbrg141 <urosdim@hotmail.com>
2020-03-30 18:35:08 +0000

    added comments

--- mondrian.py
+++ mondrian.py
@@ -3,13 +3,15 @@ WIDTH = 1024
 HEIGHT = 768
 SPLIT_LOWER = 85
 SPLIT_TOL = 1.5
-#
-#
-#
+
 def choose_color():
+    
+    ###
+    ### function for choosing a random color for rectangele 
+    ###
+    
     clrs = []
     n = np.random.randint(4,900)
-    #np.random.RandomState(seed = n)
     for i in range(n):
         rv = np.random.exponential()
         if rv < 0.15:
@@ -25,16 +27,46 @@ def choose_color():
     np.random.shuffle(clrs)
     color = np.random.choice(clrs)
     return color
+
 def choose_lines():
+    
+    ###
+    ### function for choosing a random width of line
+    ###
+
 def split_both(x,y,w,h):
+    
+    ###
+    ### functio for making a horisontal and vertical split of rectangle
+    ###
+
 def split_hor(x,y,w,h):
+
+    ###
+    ### function for making a horisontal split of rectangle, recurisve call of mondrian func
+    ###
+
 def split_ver(x,y,w,h):
+    
+    ###
+    ### function for making a vertical split of rectangle, recursive call of mondrian func
+    ###
+
 def make_svg():
-#
-#
+
+    ###
+    ### function which recives a list of string and makes
+    ###
+
+
 def mondrian(x,y,w,h):
-     
-    #recursive algorithm
+    
+    ###
+    ### functio which decides on what way a rectangle will be drawn
+    ### put those rectangles in SVGlike form in list of strings
+    ### which will be processed with make_svg(list) function
+    ###
+
     if w > WIDTH/2 and h > HEIGHT/2:
         split_both(x,y,w,h)
     elif w > WIDTH/2:
@@ -57,6 +89,10 @@ def mondrian(x,y,w,h):
         else:
         #choose_color() call
         #choose_width() call
-        #apend in list
+        #apend in list in SVGlike format
 if __name__ == '__main__':
+    ###
+    ### determinating screen resolutio in crossplatfor environment is possible with
+    ### pyGtk, pyQt5, wxPython, tKinter etc.
+    ###
     mondrian(0,0,1024,768)
