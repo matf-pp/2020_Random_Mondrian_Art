e17eb4073a96930c3d6b0adf1551993b3ba3b93e
fbrg141 <urosdim@hotmail.com>
2020-03-30 17:11:09 +0000

    choose_color function added

--- mondrian.py
+++ mondrian.py
@@ -7,6 +7,24 @@ SPLIT_TOL = 1.5
 #
 #
 def choose_color():
+    clrs = []
+    n = np.random.randint(4,900)
+    #np.random.RandomState(seed = n)
+    for i in range(n):
+        rv = np.random.exponential()
+        if rv < 0.15:
+            clrs.append("red")
+        elif rv < 0.30:
+            clrs.append("skyblue")
+        elif rv < 0.50:
+            clrs.append("yellow")
+        elif rv < 0.70:
+            clrs.append("black")
+        else:
+            clrs.append("white")
+    np.random.shuffle(clrs)
+    color = np.random.choice(clrs)
+    return color
 def choose_lines():
 def split_both(x,y,w,h):
 def split_hor(x,y,w,h):
@@ -16,7 +34,7 @@ def make_svg():
 #
 def mondrian(x,y,w,h):
      
-    #algoritam zasnovan na rekurziji
+    #recursive algorithm
     if w > WIDTH/2 and h > HEIGHT/2:
         split_both(x,y,w,h)
     elif w > WIDTH/2:
@@ -37,7 +55,8 @@ def mondrian(x,y,w,h):
         elif vsplit < h:
             split_ver(x,y,w,h)
         else:
-        #funkcija za random boju, random debljinu linije        
-        #stavljanje u svg
+        #choose_color() call
+        #choose_width() call
+        #apend in list
 if __name__ == '__main__':
     mondrian(0,0,1024,768)
