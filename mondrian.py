5b1d24d04521f6a41697ab5905ac4bb56734b80f
Uros Dimitrijevic <urosdim@hotmail.com>
2020-04-17 18:46:27 +0000

    adding make_svg function

--- mondrian.py
+++ mondrian.py
@@ -1,9 +1,9 @@
 import numpy as np
+
 WIDTH = 1024   
 HEIGHT = 768
 SPLIT_LOWER = 85
 SPLIT_TOL = 1.5
-
 
 def choose_color():
     
@@ -29,13 +29,32 @@ def choose_color():
     color = np.random.choice(clrs)
     return color
 
-#def choose_lines():
+def make_svg(rect_list):
     
     ###
-    ### function for choosing a random width of line
+    ### fuction for writing rectangles from list in .svg file
     ###
 
-def split_both(x,y,w,h, list_svg):
+    with open('art.svg', 'w') as file:
+        svg_list= ['<?xml version="1.0" encoding="utf-8"?>',
+               '<svg xmlns="http://www.w3.org/2000/svg"',
+               ' xmlns:xlink="http://www.w3.org/1999/xlink" width="{}"'
+               ' height="{}" >'.format(WIDTH, HEIGHT),
+               '<defs>',
+               '    <style type="text/css"><![CDATA[',
+               '        line {',
+               '        stroke: #000;',
+               '        stroke-width: 5px;',
+               '        }',
+               '    ]]></style>'
+               '</defs>']
+        svg_list.extend(rect_list)
+        svg_list.append('</svg>')
+        svg_str = '\n'.join(svg_list)
+        file.write(svg_str)
+
+def split_both(x,y,w,h, rect_list):
+    
     ###
     ### functio for making a horizontal and vertical split of rectangle
     ###
@@ -52,14 +71,14 @@ def split_both(x,y,w,h, list_svg):
     rand_height=round(vsp*h)
     #what's left from height
     height_left=h-rand_height
-    mondrian(x,y,rand_width,rand_height, list_svg)
-    mondrian(x+rand_width,y,width_left,rand_height, list_svg)
-    mondrian(x,y+rand_height,rand_width, height_left, list_svg)
-    mondrian(x+rand_width,y+rand_height,width_left,height_left, list_svg)
+    mondrian(x,y,rand_width,rand_height, rect_list)
+    mondrian(x+rand_width,y,width_left,rand_height, rect_list)
+    mondrian(x,y+rand_height,rand_width, height_left, rect_list)
+    mondrian(x+rand_width,y+rand_height,width_left,height_left, rect_list)
     
     
 
-def split_hor(x,y,w,h, list_svg):
+def split_hor(x,y,w,h, rect_list):
 
     ###
     ### function for making a horizontal split of rectangle, recurisve call of mondrian func
@@ -68,22 +87,23 @@ def split_hor(x,y,w,h, list_svg):
     hsp = np.random.uniform(0.33,0.68)
     rand_width = round(hsp * w)
     width_left = w - rand_width
-    mondrian(x, y, rand_width, h, list_svg)
-    mondrian(x + rand_width, y, width_left, h, list_svg)
+    mondrian(x, y, rand_width, h, rect_list)
+    mondrian(x + rand_width, y, width_left, h, rect_list)
   
 
-def split_ver(x,y,w,h, list_svg):
+def split_ver(x,y,w,h, rect_list):
     
     ###
     ### function for making a vertical split of rectangle, recursive call of mondrian func
     ###
+    
     vsp = np.random.uniform(0.33,0.68)
     rand_height = round(vsp * h)
     height_left = h - rand_height
-    mondrian(x, y, w, rand_height, list_svg)
-    mondrian(x, y + rand_height, w, height_left, list_svg)
+    mondrian(x, y, w, rand_height, rect_list)
+    mondrian(x, y + rand_height, w, height_left, rect_list)
     
-def mondrian(x,y,w,h, list_svg):
+def mondrian(x,y,w,h, rect_list):
     
     ###
     ### functio which decides on what way a rectangle will be drawn
@@ -92,11 +112,11 @@ def mondrian(x,y,w,h, list_svg):
     ###
 
     if w > WIDTH/2 and h > HEIGHT/2:
-        split_both(x,y,w,h,list_svg)
+        split_both(x,y,w,h,rect_list)
     elif w > WIDTH/2:
-        split_hor(x,y,w,h, list_svg)
+        split_hor(x,y,w,h, rect_list)
     elif h > HEIGHT/2:
-        split_ver(x,y,w,h,list_svg)
+        split_ver(x,y,w,h,rect_list)
     else:
         hsplit = np.random.uniform(SPLIT_LOWER, max(round(SPLIT_TOL * w) + 1, \
                                          SPLIT_LOWER + 1))
@@ -105,37 +125,20 @@ def mondrian(x,y,w,h, list_svg):
                        SPLIT_LOWER + 1))
         #print(vsplit)
         if hsplit < w and vsplit < h:
-            split_both(x,y,w,h, list_svg)
+            split_both(x,y,w,h, rect_list)
         elif hsplit < w:
-            split_hor(x,y,w,h, list_svg)
+            split_hor(x,y,w,h, rect_list)
         elif vsplit < h:
-            split_ver(x,y,w,h, list_svg)
+            split_ver(x,y,w,h, rect_list)
         else:
-            color = choose_color();
-            #widdth = choose_width();
-            list_svg.append('<rect x="{}" y="{}" width="{}" height="{}" style="fill: {}"/>'.format(x, y, w, h, color))
-            list_svg.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x,y,x+w,y))
-            list_svg.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x,y+h,x,y))
-            list_svg.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x+w,y+h,x+w,y))
-            list_svg.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x+w,y+h,x,y+h))
-list_svg=['<?xml version="1.0" encoding="utf-8"?>',
-               '<svg xmlns="http://www.w3.org/2000/svg"',
-               ' xmlns:xlink="http://www.w3.org/1999/xlink" width="{}"'
-               ' height="{}" >'.format(WIDTH, HEIGHT),
-               '<defs>',
-               '    <style type="text/css"><![CDATA[',
-               '        line {',
-               '        stroke: #000;',
-               '        stroke-width: 5px;',
-               '        }',
-               '    ]]></style>'
-               '</defs>']
-mondrian(0,0,1024,768, list_svg)
-list_svg.append("</svg>")
-#creating list_svg as a string with '\n' to send it as a argument to write
-list_svg = '\n'.join(list_svg)
-with open('example.svg', 'w') as fo:
-            fo.write(list_svg)
-#print(list_svg)
+            color = choose_color()
+            rect_list.append('<rect x="{}" y="{}" width="{}" height="{}" style="fill: {}"/>'.format(x, y, w, h, color))
+            rect_list.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x,y,x+w,y))
+            rect_list.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x,y+h,x,y))
+            rect_list.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x+w,y+h,x+w,y))
+            rect_list.append('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x+w,y+h,x,y+h))
 
-
+## mondrian and make_svg is called in main()
+rect_list = []
+mondrian(0, 0, WIDTH, HEIGHT, rect_list)
+make_svg(rect_list)
